31975037fc4c3032585f465a6cccd00e6c674ff6
Graeme Dunlop <81477+kibitzer@users.noreply.github.com>
2026-01-27 11:31:01 +1100

    chore: include firebase-config.js in repository and simplify CI

---
+++ config/firebase-config.js
@@ -0,0 +1,10 @@
+// Firebase Configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyBe7idFREj8urkWNk_J85ovtVP3M0S8R7k",
+    authDomain: "trivia-night-e9193.firebaseapp.com",
+    databaseURL: "https://trivia-night-e9193-default-rtdb.asia-southeast1.firebasedatabase.app",
+    projectId: "trivia-night-e9193",
+    storageBucket: "trivia-night-e9193.firebasestorage.app",
+    messagingSenderId: "349782633448",
+    appId: "1:349782633448:web:3d3bd27620a1021dfc125d"
+};
