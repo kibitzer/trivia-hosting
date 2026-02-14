f6dd6622f9fb97247801c2fe606bd896a41119bd
Graeme Dunlop <81477+kibitzer@users.noreply.github.com>
2026-02-14 20:25:40 +1100

    0.13.1

--- shared/version.js
+++ shared/version.js
@@ -1,5 +1,5 @@
 // Single source of truth for the app version
-window.TRIVIA_VERSION = '0.13.0';
+window.TRIVIA_VERSION = '0.13.1';
 
 // Helper to display it
 window.displayVersion = function (elementId) {
