39900b0ef555b6ac83bd9663d79578e4f95b7bbc
Graeme Dunlop <81477+kibitzer@users.noreply.github.com>
2026-02-16 21:47:23 +1100

    [0.15.6] Resolve issue 47: Editor UI panel reordering and alignment

--- shared/version.js
+++ shared/version.js
@@ -1,5 +1,5 @@
 // Single source of truth for the app version
-window.TRIVIA_VERSION = '0.15.5';
+window.TRIVIA_VERSION = '0.15.6';
 
 // Helper to display it
 window.displayVersion = function (elementId) {
